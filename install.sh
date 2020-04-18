a24e46329157e0c16b064151322a86aa40a5fecf
maksimken838 <maksimken838@gmail.com>
2020-04-18 17:21:16 +0300

    fix

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir ~/.config/nvim/
 cp init.vim ~/.config/nvim/init.vim
-sudo rm -r ..
+rm -r ..
